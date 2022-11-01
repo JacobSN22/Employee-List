becbc855ec0137f1e7b911c1ccee8550eeace0f5
Jacob <jacobsimoninielsen@gmail.com>
2022-11-01 09:10:39 +0100

    Employee List
    Glemte at lave et github repository så her kommer det hele

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+addEventListener('load', fetchUserData);
+    
+function fetchUserData(){
+    fetch('https://reqres.in/api/users/')
+        .then(response => response.json())
+        .then(users => {
+            console.log(users.data);
+            let output = '<h2>Lists of Users</h2>';
+            output += '<section>';
+            users.data.forEach(function(data) {
+                const {id, email, first_name, last_name, avatar} = data
+                output += `
+                    <div>
+                        <img src="${avatar}"></img> <br>
+                        <h3>${first_name} ${last_name}</h3>
+                        <a>${email}</a><br></br>
+                                               
+                    </div>
+                `;
+            });
+            output += '</ul>'
+            document.getElementById("response").innerHTML = output;
+        });
+}
